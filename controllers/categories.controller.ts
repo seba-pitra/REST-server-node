38ebdec5bb04899ba0e57989c0dc67aeb66017b9
Seba <sebastian.pitra10@gmail.com>
2023-06-28 18:08:12 -0300

    feat: create category controller

---
+++ controllers/categories.controller.ts
@@ -0,0 +1,21 @@
+
+
+export const getCategories = () => {
+  
+}
+
+export const getCategoryById = () => {
+
+}
+
+export const createCategory = () => {
+
+}
+
+export const updateCategory = () => {
+
+}
+
+export const deleteCategories = () => {
+
+}
